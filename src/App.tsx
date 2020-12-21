9846ed3d6e9e34748861a17b2420da8ba7f3c7ce
Tim Beattie <trbeattie@outlook.com>
2020-12-21 16:11:47 -0500

    Add basic routing with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, useParams } from 'react-router-dom';
+
+function Photo () {
+
+  let { photoID } = useParams<{ photoID : string }>();
+
+  return <div>Rendering photo with ID {photoID}</div>;
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello world!</p>
-    </div>
+    <Router>
+
+      <Switch>
+        <Route exact path="/">
+          Hello world!
+        </Route>
+        <Route path = "/:photoID">
+          <Photo />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
