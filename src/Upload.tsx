e31bed0295e911e4aadc7fc13b12b2af8e572e0f
Tim Beattie <trbeattie@outlook.com>
2020-12-21 19:01:22 -0500

    Re-work Upload.tsx to remove useEffect (which I don't think I was using right)

--- src/Upload.tsx
+++ src/Upload.tsx
@@ -1,30 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+
+interface UserImage {
+
+    data?: File,
+    previewSrc?: string
+}
+
+const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+    e.preventDefault();
+    console.log(e);
+
+};
 
 function Upload() {
 
-    const [image, setImage] = useState<File|null>(null);
-    const [imagePreviewSrc, setImagePreviewSrc] = useState<string | undefined>(undefined);
+    const [image, setImage] = useState<UserImage>({});
+    // const [imagePreviewSrc, setImagePreviewSrc] = useState<string | undefined>(undefined);
 
-    useEffect(() => {
+    const loadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
 
+        // Update the component's state to reflect the new image
+        const newImage = e.target.files ? e.target.files[0] : null;
+
+        // Load the image preview
         let reader = new FileReader();
         reader.onloadend = () => {
-            setImagePreviewSrc(reader.result ? reader.result.toString() : undefined);
+            setImage({
+                data: newImage ? newImage : undefined,
+                previewSrc: reader.result ? reader.result.toString() : undefined});
         };
-        if (image) reader.readAsDataURL(image);
+        if (newImage) reader.readAsDataURL(newImage);
 
-    });
+    };
 
-    console.log(image);
-    console.log(imagePreviewSrc);
+    // Submission form will be rendered only if an image has been provided
+    const submissionForm = image ? 
+    (
+        <form onSubmit={e => onFormSubmit(e)}>
+                <input type="text" placeholder="Title your image" />
+                <input type="submit" value="Submit" />
+            </form>
+    ) : null;
 
     return (
         <div>
+
             <h1>Upload an image</h1>
-            <input type="file" onChange={e => setImage(e.target.files ? e.target.files[0] : null)}/>
+            <input type="file" onChange={e => loadImage(e)}/>
+
             <div>
-                <img src={imagePreviewSrc} alt="preview" hidden={imagePreviewSrc ? false : true} />
+                <img src={image.previewSrc} alt="preview" hidden={image.previewSrc ? false : true} />
             </div>
+
+            {submissionForm}
+
         </div>
     );
 
